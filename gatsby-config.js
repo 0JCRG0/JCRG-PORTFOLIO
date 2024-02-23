05ecc2eebe8d66d1a9bfe94936f5af7e6f872324
JCRG <juancarlosrg1999@gmail.com>
2024-02-22 23:44:10 -0600

    updating

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'JCRG',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+      'Juan Reyes is a software engineer with experience in Machine Learning, Generative AI, Web Development and Data Analysis',
     siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Juan Carlos Reyes Garcia',
+        short_name: 'JCRG',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
